88ccaae3b7fb1097440a2d7d95f332cbb39de8b6
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-25 10:43:31 +0000

    clerk working

---
+++ auth.ts
@@ -0,0 +1,16 @@
+// ok so clerk is working but there is a problem comming and i dont know, fix it
+
+
+{/* 
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_Y2xpbWJpbmctdHVya2V5LTM5LmNsZXJrLmFjY291bnRzLmRldiQ
+CLERK_SECRET_KEY=sk_test_OzkW5r0BNmCduz6zP4m0GZfoWdf9RV956BlMRwoZz7
+
+DATABASE_URL=postgresql://Test-db-for_nancy_owner:HprTB84oWndL@ep-calm-truth-a1896lcv.ap-southeast-1.aws.neon.tech/Test-db-for_nancy?sslmode=require
+
+NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME=
+CLOUDINARY_API_KEY=
+CLOUDINARY_API_SECRET=
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+
+    */}
