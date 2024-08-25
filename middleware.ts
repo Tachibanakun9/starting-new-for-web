a71fb833fe7cb087b2914a35f0290b7320aac4c8
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-25 10:06:36 +0000

    all pages created

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-  public;
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+   '/',
+   '/about-us',
+   '/contact-us',
+   '/privacy-policy',
+   '/terms-and-condition',
+   '/testing-page'
+  
+  ])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
 })
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
