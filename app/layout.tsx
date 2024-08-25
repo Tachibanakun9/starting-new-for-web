cb2da678975f25302e290073dee34f5c417f7632
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-25 14:51:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
